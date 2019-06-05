960dc930c35f478acd5b796c008e1890df5dfc55
yukuima <58396683@qq.com>
2019-06-05 21:09:29 +0800

    commit

---
+++ src/main/java/com/myk/learning/nsq/config/SpringConfiguration.java
@@ -0,0 +1,48 @@
+package com.myk.learning.nsq.config;
+
+import com.myk.learning.nsq.service.impl.MqConsumerByChannelServiceImpl;
+import com.myk.learning.nsq.service.impl.MqConsumerServiceImpl;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Spring configuration
+ * Created in 2018.11.14
+ *
+ * @author myk
+ */
+@Configuration
+public class SpringConfiguration {
+
+    /**
+     * The Topic.
+     */
+    @Value("${nsq.topic}")
+    String topic;
+    /**
+     * The Address.
+     */
+    @Value("${nsq.address}")
+    String nsqAddress;
+
+    @Value("${nsq.port}")
+    Integer nsqPort;
+    @Value("${nsq.thread.count}")
+    Integer nsqThreadCount;
+
+    /**
+     * Mq consumer service mq consumer service.
+     *
+     * @return the mq consumer service
+     */
+    @Bean(initMethod = "mqConsumer")
+    public MqConsumerServiceImpl mqConsumerService() {
+        return new MqConsumerServiceImpl(topic, nsqAddress, nsqPort, nsqThreadCount);
+    }
+
+    @Bean(initMethod = "mqConsumer")
+    public MqConsumerByChannelServiceImpl mqConsumerByChannelService() {
+        return new MqConsumerByChannelServiceImpl(topic, nsqAddress, nsqPort, nsqThreadCount);
+    }
+}
---
+++ src/main/java/com/myk/learning/nsq/service/MqConsumerService.java
@@ -0,0 +1,14 @@
+package com.myk.learning.nsq.service;
+
+/**
+ * The interface Mq product service.
+ * Created in 2018.11.13
+ *
+ * @author myk
+ */
+public interface MqConsumerService {
+    /**
+     * Mq consumer.
+     */
+    void mqConsumer();
+}
